8e7e1cd4c3563d4d18984f84df50e835d1006a62
Daniel Tull <dt@danieltull.co.uk>
2013-05-07 09:04:25 +0100

    Add ISO8601DateFormatter

--- DCTValueTransformers/DCTISO8601ToDateValueTransformer.m
+++ DCTValueTransformers/DCTISO8601ToDateValueTransformer.m
@@ -7,7 +7,33 @@
 //
 
 #import "DCTISO8601ToDateValueTransformer.h"
+#import "ISO8601DateFormatter.h"
 
 @implementation DCTISO8601ToDateValueTransformer
+
++ (ISO8601DateFormatter *)dateFormatter {
+	static ISO8601DateFormatter *dateFormatter;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		dateFormatter = [ISO8601DateFormatter new];
+	});
+	return dateFormatter;
+}
+
++ (Class)transformedValueClass {
+	return [NSDate class];
+}
+
++ (BOOL)allowsReverseTransformation {
+	return YES;
+}
+
+- (id)transformedValue:(NSString *)value {
+	return [[[self class] dateFormatter] dateFromString:value];
+}
+
+- (id)reverseTransformedValue:(NSDate *)value {
+	return [[[self class] dateFormatter] stringFromDate:value];
+}
 
 @end
