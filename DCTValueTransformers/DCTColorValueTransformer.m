404c73ecd01d2f69cb050fb0027cd77206200ab3
Daniel Tull <dt@danieltull.co.uk>
2016-03-22 08:59:02 +0000

    Fix warning about implicit conversion

--- DCTValueTransformers/DCTColorValueTransformer.m
+++ DCTValueTransformers/DCTColorValueTransformer.m
@@ -40,9 +40,9 @@ typedef NSColor DCTColorValueTransformerColor;
 	unsigned int color;
 	[scanner scanHexInt:&color];
 
-	CGFloat red = ((color & 0xFF0000) >> 16) / 255.0f;
-	CGFloat green = ((color & 0x00FF00) >>  8) / 255.0f;
-	CGFloat blue = (color & 0x0000FF) / 255.0f;
+	CGFloat red = ((color & 0xFF0000) >> 16) / 255.0;
+	CGFloat green = ((color & 0x00FF00) >>  8) / 255.0;
+	CGFloat blue = (color & 0x0000FF) / 255.0;
 
 	return [DCTColorValueTransformerColor colorWithRed:red green:green blue:blue alpha:1.0];
 }
